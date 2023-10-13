3e84a8bdde20d0310b5ec0ade4c47ee8cba86f25
NicolasCBV <nicolascleiton700e@gmail.com>
2023-10-12 23:00:34 -0300

    feat(banco de dados): definindo o prisma
    Commit para definir as configurações necessárias para executar o prisma no servidor.

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
 import { AppService } from 'src/app/services/app.service';
+import { PrismaModule } from '@infra/storages/db/prisma/prisma.module';
 
 @Module({
+	imports: [PrismaModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
