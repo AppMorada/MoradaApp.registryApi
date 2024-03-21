2391620f9a2b2268eff7ea1c2df96f80e8a8f2e4
NicolasCBV <nicolascleiton700e@gmail.com>
2024-03-20 23:18:35 -0300

    fix: corrigir init.sql

--- example/init.sql
+++ example/init.sql
@@ -29,11 +29,6 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 
 COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
 
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
 --
 -- Name: community_infos; Type: TABLE; Schema: public; Owner: postgres
 --
