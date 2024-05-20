569c2e58ddce0f51c33d4e9ee14df082e4bdd64a
Nícolas <92616145+NicolasCBV@users.noreply.github.com>
2024-05-20 18:10:14 -0300

    feat(pubsub): adicionar telemetria no modulo de pubsub (#169)
    ## Resuma o seu PR em até 5 linhas:
    Adicionar recurso de telemetria no módulo de pubsub
    
    ## Selecione o tipo de mudanças feitas:
    - [ ] Bug fix
    - [X] Novas funcionalidades
    - [ ] Atualização da documentação
    - [ ] Atualização dos testes
    - [ ] Modificações relacionadas a CI/CD
    - [ ] Refatoração do código 
    - [ ] Outro:
    
    ## Este PR gera alguma breaking change? Se sim, onde?
    Não

--- src/infra/events/pubsub/google/publishers/deleteCondominium.ts
+++ src/infra/events/pubsub/google/publishers/deleteCondominium.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { GooglePubSubService } from '../pubsub.service';
 import { OnEvent } from '@nestjs/event-emitter';
 import { EVENT_ID } from '@infra/events/ids';
@@ -7,23 +7,42 @@ import {
 	IDeleteCondominiumProps,
 } from '@app/publishers/deleteCondominium';
 import { CondominiumMapper } from '@app/mapper/condominium';
+import { TRACE_ID, TraceHandler } from '@infra/configs/tracing';
 
 @Injectable()
 export class DeleteCondominiumGooglePublisher
 implements DeleteCondominiumPublisher
 {
-	constructor(private readonly pubSubService: GooglePubSubService) {}
+	constructor(
+		private readonly pubSubService: GooglePubSubService,
+		@Inject(TRACE_ID)
+		private readonly trace: TraceHandler,
+	) {}
 
 	private readonly topicName = 'delete_condominium';
 
 	@OnEvent(EVENT_ID.PUBSUB.DELETE_CONDOMINIUM, { async: true })
 	async publish(input: IDeleteCondominiumProps): Promise<void> {
+		const tracer = this.trace.getTracer(EVENT_ID.TRACE.PUBSUB);
+		const span = tracer.startSpan(`topic: ${this.topicName}`);
+
 		const topic = this.pubSubService.topic(this.topicName);
-		if (process.env.NODE_ENV !== 'production' || !topic.exists()) return;
+		const topicExist = await topic.exists();
+		if (process.env.NODE_ENV !== 'production' || !topicExist[0]) {
+			span.setAttribute('published', 'false');
+			return span.end();
+		}
 
 		const condominium = CondominiumMapper.toObject(input.condominium);
-		await topic.publishMessage({
-			json: { condominium, deletedAt: input.deletedAt },
-		});
+		const data = Buffer.from(
+			JSON.stringify({
+				condominium,
+				deletedAt: input.deletedAt,
+			}),
+		);
+		await topic.publishMessage({ data });
+
+		span.setAttribute('published', 'true');
+		span.end();
 	}
 }
--- src/infra/events/pubsub/google/publishers/deleteMember.ts
+++ src/infra/events/pubsub/google/publishers/deleteMember.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { GooglePubSubService } from '../pubsub.service';
 import { OnEvent } from '@nestjs/event-emitter';
 import { EVENT_ID } from '@infra/events/ids';
@@ -6,23 +6,39 @@ import {
 	DeleteMemberPublisher,
 	IDeleteMemberProps,
 } from '@app/publishers/deleteMember';
+import { TRACE_ID, TraceHandler } from '@infra/configs/tracing';
 
 @Injectable()
 export class DeleteMemberGooglePublisher implements DeleteMemberPublisher {
-	constructor(private readonly pubSubService: GooglePubSubService) {}
+	constructor(
+		private readonly pubSubService: GooglePubSubService,
+		@Inject(TRACE_ID)
+		private readonly trace: TraceHandler,
+	) {}
 
 	private readonly topicName = 'delete_member';
 
 	@OnEvent(EVENT_ID.PUBSUB.DELETE_MEMBER, { async: true })
 	async publish(input: IDeleteMemberProps): Promise<void> {
-		const topic = this.pubSubService.topic(this.topicName);
-		if (process.env.NODE_ENV !== 'production' || !topic.exists()) return;
+		const tracer = this.trace.getTracer(EVENT_ID.TRACE.PUBSUB);
+		const span = tracer.startSpan(`topic: ${this.topicName}`);
 
-		await topic.publishMessage({
-			json: {
+		const topic = this.pubSubService.topic(this.topicName);
+		const topicExists = await topic.exists();
+		if (process.env.NODE_ENV !== 'production' || !topicExists[0]) {
+			span.setAttribute('published', 'false');
+			return span.end();
+		}
+
+		const data = Buffer.from(
+			JSON.stringify({
 				uniqueRegistry: input.uniqueRegistry,
 				deletedAt: input.deletedAt,
-			},
-		});
+			}),
+		);
+		await topic.publishMessage({ data });
+
+		span.setAttribute('published', 'true');
+		span.end();
 	}
 }
